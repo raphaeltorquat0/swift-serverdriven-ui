992e07c7db71d9f5539c53f3cdb83897ce9759ab
Raphael Torquato <89878688+raphaeltorquat0@users.noreply.github.com>
2022-06-06 16:13:08 -0300

    Initial Commit

---
+++ swift-serverdriven-ui/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swift-serverdriven-ui
+//
+//  Created by Raphael Torquato on 06/06/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swift-serverdriven-ui/swift_serverdriven_uiApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_serverdriven_uiApp.swift
+//  swift-serverdriven-ui
+//
+//  Created by Raphael Torquato on 06/06/22.
+//
+
+import SwiftUI
+
+@main
+struct swift_serverdriven_uiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
